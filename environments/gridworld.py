650925114c1c4c86e53c3c09c69e9f5e890c3d4a
mirqwa <kimrodrikwa@gmail.com>
2024-05-25 07:09:58 +0300

    complete basic environment

--- environments/gridworld.py
+++ environments/gridworld.py
@@ -1,8 +1,12 @@
+import typing
+
 import numpy as np
 
 
 class GridWorld:
-    def __init__(self, rows, columns, max_iterations=3) -> None:
+    def __init__(
+        self, rows: int, columns: int, max_iterations: typing.Optional[int] = 3
+    ) -> None:
         self.max_row = rows - 1
         self.max_column = columns - 1
         self.values = np.zeros((rows, columns))
@@ -11,30 +15,29 @@ class GridWorld:
         self.max_iterations = max_iterations
         self.valid_actions = ["up", "right", "down", "left"]
 
-    def take_actions_for_a_state(self, row, column) -> None:
+    def take_actions_for_a_state(self, row: int, column: int) -> float:
+        state_value = 0
         for action in self.valid_actions:
             if action == "up":
-                new_state = (max(row - 1, 0), column)
+                next_state = (max(row - 1, 0), column)
             elif action == "right":
-                new_state = (row, min(column + 1, self.max_column))
+                next_state = (row, min(column + 1, self.max_column))
             elif action == "down":
-                new_state = (min(row + 1, self.max_row), column)
+                next_state = (min(row + 1, self.max_row), column)
             else:
-                new_state = (row, max(column - 1, 0))
-            print((row, column), action, new_state)
+                next_state = (row, max(column - 1, 0))
+            state_value += self.get_state_action_value(next_state)
+        return state_value
 
-    def get_action_reward(self, next_state) -> None:
-        pass
+    def get_state_action_value(self, next_state: typing.Tuple[int]) -> float:
+        return 0.25 * (-1 + self.values[next_state])
 
     def iterate_states(self) -> None:
-        for i in range(self.max_iterations):
+        for _ in range(self.max_iterations):
+            next_values = np.zeros((self.max_row + 1, self.max_column + 1))
             for row in range(self.max_row + 1):
                 for col in range(self.max_column + 1):
                     if (row, col) in self.terminal_states:
                         continue
-                    self.take_actions_for_a_state(row, col)
-
-
-if __name__ == "__main__":
-    env = GridWorld(4, 4)
-    env.iterate_states()
+                    next_values[(row, col)] = self.take_actions_for_a_state(row, col)
+            self.values = next_values
