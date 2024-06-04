2830839fb3a54cf083e22bdc664e830772adf4bd
mirqwa <kimrodrikwa@gmail.com>
2024-06-04 23:40:16 +0300

    add random walk environment

---
+++ environments/random_walk.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+np.random.seed(0)
+
+
+class RandomWalk:
+    def __init__(self, num_of_states: int, initial_state: int, max_steps: int) -> None:
+        self.num_of_states = num_of_states
+        self.terminal_states_rewards = {
+            num_of_states: 1,
+            0: -1,
+        }
+        self.current_state = initial_state
+        self.possible_steps = [i for i in range(-max_steps, max_steps + 1) if i != 0]
+        self.steps_probabilities = [
+            1 / (len(self.possible_steps)) for _ in self.possible_steps
+        ]
+        self.terminated = False
+        self.reward = 0
+
+    def take_a_step(self) -> None:
+        steps = np.random.choice(self.possible_steps, 1, p=self.steps_probabilities)[0]
+        self.current_state = (
+            min(self.current_state + steps, self.num_of_states)
+            if steps > 0
+            else max(self.current_state + steps, 0)
+        )
+        if self.current_state in self.terminal_states_rewards.keys():
+            self.reward = self.terminal_states_rewards[self.current_state]
+            self.terminated = True
